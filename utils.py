a69a5830b9533454f9480211044eb07a901b05ff
Luis Fernandez <luisfdez1992@gmail.com>
2025-10-16 21:05:41 -0600

    Add types

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
-def suma(n1, n2):
+def suma(n1: int, n2: int) -> int:
     return n1 + n2
 
-def resta(n1, n2):
+def resta(n1: int, n2: int) -> int:
     return n1 - n2
 
-def multi(n1, n2):
+def multi(n1: int, n2: int) -> int:
     return n1 * n2
 
-def division(n1, n2):
+def division(n1: int, n2: int) -> int:
     return n1 / n2
