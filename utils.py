3a177de667857836c9264922ba122a3fe8ff4da0
Luis Fernandez <luisfdez1992@gmail.com>
2025-10-16 21:06:52 -0600

    Add docstring

--- utils.py
+++ utils.py
@@ -1,11 +1,15 @@
 def suma(n1: int, n2: int) -> int:
+    """Operations"""
     return n1 + n2
 
 def resta(n1: int, n2: int) -> int:
+    """Operations"""
     return n1 - n2
 
 def multi(n1: int, n2: int) -> int:
+    """Operations"""
     return n1 * n2
 
 def division(n1: int, n2: int) -> int:
+    """Operations"""
     return n1 / n2
